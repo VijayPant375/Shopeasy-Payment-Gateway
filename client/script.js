833d8fe08baf74778f7e6792ec94ac56cafdd724
Vijay Pant <vijaypant486@gmail.com>
2026-03-12 23:53:00 +0530

    feat: add product landing page with basic layout

---
+++ client/script.js
@@ -0,0 +1,4 @@
+document.getElementById('buyBtn').addEventListener('click', () => {
+  // Placeholder - will be replaced with Razorpay logic in next commit
+  alert('Payment integration coming soon!');
+});
